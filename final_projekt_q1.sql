dc1761ce3e604a5320d2ee37ef5bcb0c65c4dce3
Martin Minčeff <martinekk.b@gmail.com>
2023-04-27 17:48:04 +0200

    Formatting changes, new aliases as requested

--- final_projekt_q1.sql
+++ final_projekt_q1.sql
@@ -8,45 +8,42 @@
 
 SELECT 
 	*
-FROM t_martinminceff_sql_primary_final tmspf ;
+FROM t_martinminceff_sql_primary_final pf;
 
 
-SELECT DISTINCT 
-	tmspf.industry_branch_code,
-	tmspf .industry_name ,
-	tmspf .payroll_year ,
-	tmspf .avg_month_payroll ,
-	tmspf2 .payroll_year AS payroll_nextyear ,
-	tmspf2 .avg_month_payroll AS payroll_nextyear ,
-	round(( tmspf2 .avg_month_payroll  - tmspf .avg_month_payroll ) / tmspf .avg_month_payroll * 100, 2 ) as avg_payroll_growth
-FROM t_martinminceff_sql_primary_final tmspf
-JOIN t_martinminceff_sql_primary_final tmspf2 
-	ON tmspf .industry_branch_code = tmspf2 .industry_branch_code 
-	AND tmspf .payroll_year + 1 = tmspf2 .payroll_year 
-;
-
+SELECT DISTINCT
+	pf.industry_branch_code,
+	pf.industry_name,
+	pf.payroll_year,
+	pf.avg_month_payroll,
+	pf2.payroll_year AS payroll_nextyear,
+	pf2.avg_month_payroll AS payroll_nextyear,
+	ROUND((pf2.avg_month_payroll - pf.avg_month_payroll) / pf.avg_month_payroll * 100, 2) as avg_payroll_growth
+FROM t_martinminceff_sql_primary_final pf
+JOIN t_martinminceff_sql_primary_final pf2
+	ON pf.industry_branch_code = pf2.industry_branch_code
+	AND pf.payroll_year + 1 = pf2.payroll_year;
 
 
 WITH avg_payroll_dude AS
 (
-	SELECT DISTINCT 
-		tmspf.industry_branch_code,
-		tmspf .industry_name ,
-		tmspf .payroll_year ,
-		tmspf .avg_month_payroll ,
-		tmspf2 .payroll_year AS payroll_nextyear ,
-		tmspf2 .avg_month_payroll AS avg_monthpayroll_nextyear ,
-		round(( tmspf2 .avg_month_payroll  - tmspf .avg_month_payroll ) / tmspf .avg_month_payroll * 100, 2 ) as avg_payroll_growth
-	FROM t_martinminceff_sql_primary_final tmspf
-	JOIN t_martinminceff_sql_primary_final tmspf2 
-		ON tmspf .industry_branch_code = tmspf2 .industry_branch_code 
-		AND tmspf .industry_name = tmspf2 .industry_name 
-		AND tmspf .payroll_year + 1 = tmspf2 .payroll_year 
+SELECT DISTINCT
+	pf.industry_branch_code,
+	pf.industry_name,
+	pf.payroll_year,
+	pf.avg_month_payroll,
+	pf2.payroll_year AS payroll_nextyear,
+	pf2.avg_month_payroll AS month_payroll_nextyear,
+	ROUND((pf2.avg_month_payroll - pf.avg_month_payroll) / pf.avg_month_payroll * 100, 2) as avg_payroll_growth
+FROM t_martinminceff_sql_primary_final pf
+JOIN t_martinminceff_sql_primary_final pf2
+	ON pf.industry_branch_code = pf2.industry_branch_code
+	AND pf.payroll_year + 1 = pf2.payroll_year
 )
-SELECT 
-	industry_branch_code ,
-	industry_name ,
+SELECT
+	industry_branch_code,
+	industry_name,
 	ROUND(AVG(avg_payroll_growth), 2) AS alltime_payroll_growth
 FROM avg_payroll_dude
-GROUP BY industry_branch_code 
-ORDER BY industry_branch_code ;
+GROUP BY industry_branch_code
+ORDER BY industry_branch_code;
